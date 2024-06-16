3ea3a65da56135f87ad7f425599145569131c4cb
Nathan Mande <neodarksoulink@gmail.com>
2024-06-16 04:12:28 +0200

    feat: adds modules & resources types

--- packages/reactive/src/common/interfaces/module.interface.ts
+++ packages/reactive/src/common/interfaces/module.interface.ts
@@ -1,3 +1,19 @@
+/**
+ * @description Module representation.
+ *
+ * @important Each module should `implement` this interface.
+ */
 export interface Module {
+	/**
+	 * @description Module initializer method.
+	 *
+	 * Can be used for direct initialization (when constructing `quick-three`) or manual (later in the code).
+	 */
 	init(...props: any[]): void;
+	/**
+	 * @description Module disposal.
+	 *
+	 * Used to terminate the Module execution and free memory.
+	 */
+	dispose(): void;
 }
