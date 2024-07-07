ad4b1fce11d9873f2073f5c475189f94ebb2c075
Nathan Mande <neodarksoulink@gmail.com>
2024-07-07 20:16:02 +0200

    chore: use correct pre-commit script

---
+++ .changeset/perfect-keys-shake.md
@@ -0,0 +1,5 @@
+---
+"@quick-threejs/reactive": patch
+---
+
+v0.1.6 `@quick-threejs/reactive`documentation improvement.
--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,4 @@
 export default {
-	"**/*.{js,jsx,ts,tsx}": ["eslint --max-warnings=0", "prettier --write"],
-	"**/*.{html,json,css,scss,md,mdx}": ["prettier -w"]
+	"**/*.{js,jsx,ts,tsx}": ["pnpm run lint", "pnpm run format"],
+	"**/*.{html,json,css,scss,md,mdx}": ["pnpm run format"]
 };
