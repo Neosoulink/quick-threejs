c3f1ad7a2fd633ca340900313ccca0a374f35c06
Nathan Mande <neodarksoulink@gmail.com>
2024-07-07 21:15:19 +0200

    chore(conf): skip  for releases

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,7 @@
+const isReleasing = process.env.CI_RELEASE === "true";
+
 export default {
-	"**/*.{js,jsx,ts,tsx,html,json,css,scss,md,mdx}": [
-		"pnpm run lint",
-		"pnpm run format"
-	]
+	"**/*.{js,jsx,ts,tsx,html,json,css,scss,md,mdx}": isReleasing
+		? []
+		: ["pnpm run lint", "pnpm run format"]
 };
