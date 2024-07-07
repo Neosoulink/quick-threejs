1bed40b48342318a86d2d50681e4f30917fdf965
Nathan Mande <neodarksoulink@gmail.com>
2024-07-07 21:24:26 +0200

    chore(conf): use custom pre-commit script

--- lint-staged.config.mjs
+++
@@ -1,7 +0,0 @@
-const isReleasing = process.env.CI_RELEASE === "true";
-
-export default {
-	"**/*.{js,jsx,ts,tsx,html,json,css,scss,md,mdx}": isReleasing
-		? []
-		: ["pnpm run lint", "pnpm run format"]
-};
