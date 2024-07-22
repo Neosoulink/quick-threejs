7507a34f0de7c4cd185a2f02a997491d2d5a46c9
Nathan Mande <neodarksoulink@gmail.com>
2024-07-22 22:42:35 +0200

    feat: export types
    ### Description
    
    - Add new types & export them
      - Add `NonNever`
      - Add `Methods`
      - Add `Properties`

--- shared/utils/src/types/object.type.ts
+++ shared/utils/src/types/object.type.ts
@@ -1,5 +1,22 @@
 import { EulerOrder } from "three";
 
+/** @description Serialized `position` format */
 export type SerializedPosition = [number, number, number];
 
+/** @description Serialized `rotation` format */
 export type SerializedRotation = [number, number, number, EulerOrder];
+
+/** @description Utility type to exclude properties with the type `never` */
+export type NonNever<T extends object> = {
+	[K in keyof T as T[K] extends never ? never : K]: T[K];
+};
+
+/** @description Utility type to extract only **methods** of an `object` */
+export type Methods<T extends object> = NonNever<{
+	[K in keyof T]: T[K] extends (...args: any[]) => any ? T[K] : never;
+}>;
+
+/** @description Utility type to extract only **properties** of an `object` */
+export type Properties<T extends object> = NonNever<{
+	[K in keyof T]: T[K] extends (...args: any[]) => any ? never : T[K];
+}>;
