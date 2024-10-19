339bd72a0fa3f8c50941a8a2f8defaf9c5a5cf66
Nathan Mande <neodarksoulink@gmail.com>
2024-10-19 17:35:50 +0200

    fix(configs): disable `eslint` `"@typescript-eslint/no-duplicate-enum-values`

--- shared/config/eslint.js
+++ shared/config/eslint.js
@@ -35,7 +35,8 @@ module.exports = {
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/ban-ts-comment": "off",
 		"@typescript-eslint/no-var-requires": "off",
-		"@typescript-eslint/no-unused-vars": "off"
+		"@typescript-eslint/no-unused-vars": "off",
+		"@typescript-eslint/no-duplicate-enum-values": "off"
 	},
 	ignorePatterns: [
 		"**/__tests__/*",
