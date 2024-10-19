3a49250bf3a6c42928f73e42d10e463157cd13c8
Nathan Mande <neodarksoulink@gmail.com>
2024-10-19 17:48:49 +0200

    fix(reactive): correct unsued expressions

--- packages/reactive/src/core/app/camera/camera.module.ts
+++ packages/reactive/src/core/app/camera/camera.module.ts
@@ -57,7 +57,7 @@ export class CameraModule implements Module {
 	}
 
 	public instance(value?: Camera) {
-		if (value) this.component.instance;
+		if (value) this.component.instance = value;
 		return this.component.instance;
 	}
 
