fb4136b632be41e80af0df40d2b52543cac29f8c
Nathan Mande <neodarksoulink@gmail.com>
2024-10-19 17:30:29 +0200

    chore(deps): dependencies correction

--- shared/config/eslint.js
+++ shared/config/eslint.js
@@ -35,7 +35,7 @@ module.exports = {
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/ban-ts-comment": "off",
 		"@typescript-eslint/no-var-requires": "off",
-		"@typescript-eslint/no-unused-vars":  "off",
+		"@typescript-eslint/no-unused-vars": "off"
 	},
 	ignorePatterns: [
 		"**/__tests__/*",
