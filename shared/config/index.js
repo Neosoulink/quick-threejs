73f97cb179b2163db4f7321bc3b2e61a8b943abc
Nathan Mande <neodarksoulink@gmail.com>
2024-10-20 13:13:05 +0200

    fix(configs): improve configs accessibility

--- shared/config/index.js
+++ shared/config/index.js
@@ -1,6 +1,6 @@
 const eslint = require("./eslint");
-const prettier = require("./prettier.mjs");
+const prettier = require("./prettier");
 const typescript = require("./typescript.json");
-const vite = require("./vite");
+const vite = require("./vite.mjs");
 
 module.exports = { eslint, prettier, typescript, vite };
