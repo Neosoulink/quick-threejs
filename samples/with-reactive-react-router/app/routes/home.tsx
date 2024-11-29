931998582ad699c91d89943b2d3f25f12a965ee1
Nathan Mande <neodarksoulink@gmail.com>
2024-11-29 12:41:30 +0200

    fix(react-router): correct `reactive` importation

--- samples/with-reactive-react-router/app/routes/home.tsx
+++ samples/with-reactive-react-router/app/routes/home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { register, RegisterModule } from "@quick-threejs/reactive";
+import type { RegisterModule } from "@quick-threejs/reactive";
 
 export function meta() {
 	return [
@@ -12,28 +12,30 @@ export default function Home() {
 	const [app, setApp] = useState<RegisterModule | undefined>();
 
 	useEffect(() => {
-		if (!app && typeof window !== "undefined")
-			register({
-				location: new URL(
-					"../core/main.worker.ts",
-					import.meta.url
-				) as unknown as string,
-				enableDebug: true,
-				axesSizes: 5,
-				gridSizes: 10,
-				withMiniCamera: true,
-				onReady: (app) => {
-					app
-						.gui()
-						?.add({ torusX: 0 }, "torusX")
-						.step(0.01)
-						.onChange((value: any) => {
-							app.worker()?.postMessage({ type: "torus-x-gui-event", value });
-						});
+		import("@quick-threejs/reactive").then(({ register }) => {
+			if (!app && typeof window !== "undefined")
+				register({
+					location: new URL(
+						"../core/main.worker.ts",
+						import.meta.url
+					) as unknown as string,
+					enableDebug: true,
+					axesSizes: 5,
+					gridSizes: 10,
+					withMiniCamera: true,
+					onReady: (app) => {
+						app
+							.gui()
+							?.add({ torusX: 0 }, "torusX")
+							.step(0.01)
+							.onChange((value: any) => {
+								app.worker()?.postMessage({ type: "torus-x-gui-event", value });
+							});
 
-					setApp(app);
-				}
-			});
+						setApp(app);
+					}
+				});
+		});
 	}, []);
 
 	return <div />;
